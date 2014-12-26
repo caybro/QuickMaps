6aabf9701dbeffe76de516c609c2b4aeb2dfca15
Lukáš Tinkl <lukas@kde.org>
2014-12-26 01:27:36 +0100

    translation update

--- src/translations/quickmaps_cs.ts
+++ src/translations/quickmaps_cs.ts
@@ -2,21 +2,91 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="cs_CZ">
 <context>
+    <name></name>
+    <message numerus="yes">
+        <location filename="../QuickRouting.qml" line="50"/>
+        <source>%n day(s)</source>
+        <translation>
+            <numerusform>1 den</numerusform>
+            <numerusform>%n dny</numerusform>
+            <numerusform>%n dní</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../QuickRouting.qml" line="52"/>
+        <source>%n hour(s)</source>
+        <translation>
+            <numerusform>1 hodina</numerusform>
+            <numerusform>%n hodiny</numerusform>
+            <numerusform>%n hodin</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../QuickRouting.qml" line="54"/>
+        <source>%n minute(s)</source>
+        <translation>
+            <numerusform>1 minuta</numerusform>
+            <numerusform>%n minuty</numerusform>
+            <numerusform>%n minut</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../QuickRouting.qml" line="56"/>
+        <source>%n second(s)</source>
+        <translation>
+            <numerusform>1 vteřina</numerusform>
+            <numerusform>%n vteřiny</numerusform>
+            <numerusform>%n vteřin</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
     <name>QApplication</name>
     <message>
-        <location filename="../main.cpp" line="26"/>
+        <location filename="../main.cpp" line="43"/>
         <source>Simple maps application based on QML</source>
         <translation>Jednoduchá mapová aplikace založená na QML</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="30"/>
+        <location filename="../main.cpp" line="47"/>
         <source>Latitude to start with, in decimal degrees</source>
         <translation>Počáteční zěm. šířka, v desetinných stupních</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="33"/>
+        <location filename="../main.cpp" line="50"/>
         <source>Longitude to start with, in decimal degrees</source>
         <translation>Počáteční zěm. délka, v desetinných stupních</translation>
+    </message>
+</context>
+<context>
+    <name>QuickMap</name>
+    <message>
+        <location filename="../QuickMap.qml" line="98"/>
+        <source>What&apos;s here?</source>
+        <translation>Co je zde?</translation>
+    </message>
+    <message>
+        <location filename="../QuickMap.qml" line="107"/>
+        <source>Directions from this place</source>
+        <translation>Navigovat odsud</translation>
+    </message>
+    <message>
+        <location filename="../QuickMap.qml" line="118"/>
+        <source>Directions to this place</source>
+        <translation>Navigovat sem</translation>
+    </message>
+</context>
+<context>
+    <name>QuickRouting</name>
+    <message>
+        <location filename="../QuickRouting.qml" line="30"/>
+        <source>Route measures %1 kilometers and will take %2.</source>
+        <translation>Cesta měří %1 kilometrů a zabere %2.</translation>
+    </message>
+    <message>
+        <location filename="../QuickRouting.qml" line="34"/>
+        <source>Error occurred: %1</source>
+        <translation>Nastala chyba: %1</translation>
     </message>
 </context>
 <context>
@@ -30,22 +100,126 @@
         <translation type="vanished">Souřadnice</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="308"/>
+        <location filename="../main.qml" line="235"/>
+        <source>Zoom Out</source>
+        <translation>Oddálit</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="250"/>
+        <source>Zoom In</source>
+        <translation>Přiblížit</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="262"/>
         <source>&amp;Quit</source>
         <translation>U&amp;končit</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="309"/>
+        <location filename="../main.qml" line="263"/>
         <source>Exit Application</source>
         <translation>Ukončit aplikaci</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="317"/>
-        <source>&amp;Go</source>
-        <translation>Pře&amp;jít</translation>
+        <location filename="../main.qml" line="271"/>
+        <source>&amp;Find</source>
+        <translation>Na&amp;jít</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="332"/>
+        <location filename="../main.qml" line="301"/>
+        <source>My Location</source>
+        <translation>Moje umístění</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="302"/>
+        <source>Display my location on the map</source>
+        <translation>Zobrazit mé umístění na mapě</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="321"/>
+        <source>Directions</source>
+        <translation>Pokyny</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="359"/>
+        <source>Start</source>
+        <translation>Začátek</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="359"/>
+        <source>Search for places, addresses and locations</source>
+        <translation>Hledat místa, adresy a body zájmu</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="394"/>
+        <source>Swap</source>
+        <translation>Prohodit</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="395"/>
+        <source>Swap Start and Destination</source>
+        <translation>Prohodit start a cíl</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="410"/>
+        <source>Destination</source>
+        <translation>Cíl</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="482"/>
+        <source>Drive</source>
+        <translation>Jet autem</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="494"/>
+        <source>Walk</source>
+        <translation>Jít pěšky</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="506"/>
+        <source>Public Transport</source>
+        <translation>Veřejná doprava</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="522"/>
+        <source>&amp;Fastest route</source>
+        <translation>&amp;Nejrychlejší cesta</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="529"/>
+        <source>&amp;Shortest route</source>
+        <translation>Nejkratší ce&amp;sta</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="536"/>
+        <source>Toll roads</source>
+        <translation>Placené úseky</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="544"/>
+        <source>Highways</source>
+        <translation>Dálnice</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="552"/>
+        <source>Tunnels</source>
+        <translation>Tunely</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="560"/>
+        <source>Unpaved roads</source>
+        <translation>Nezpevněné cesty</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="568"/>
+        <source>Ferries</source>
+        <translation>Trajekty</translation>
+    </message>
+    <message>
+        <source>&amp;Go</source>
+        <translation type="vanished">Pře&amp;jít</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="286"/>
         <source>&amp;Back</source>
         <translation>&amp;Zpět</translation>
     </message>
@@ -54,22 +228,21 @@
         <translation type="vanished">&amp;Vpřed</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="347"/>
         <source>&amp;Home</source>
-        <translation>&amp;Domů</translation>
+        <translation type="vanished">&amp;Domů</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="362"/>
+        <location filename="../main.qml" line="338"/>
         <source>View &amp;Fullscreen</source>
         <translation>Zobrazit na &amp;celou obrazovku</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="382"/>
         <source>Query:</source>
-        <translation>Dotaz:</translation>
+        <translation type="vanished">Dotaz:</translation>
     </message>
     <message numerus="yes">
-        <location filename="../main.qml" line="297"/>
+        <location filename="../main.qml" line="195"/>
+        <location filename="../main.qml" line="199"/>
         <source>Query returned %n item(s)</source>
         <translation>
             <numerusform>Dotaz vrátil 1 položku</numerusform>
